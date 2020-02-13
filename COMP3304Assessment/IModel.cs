4e947d16e59897d0311b741a37b246ceecb9d484
Ben Rose <rosb1_17@uni.worc.ac.uk>
2020-02-13 15:40:40 +0000

    Initial

---
+++ COMP3304Assessment/IModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace COMP3304Assessment
+{
+    interface IModel
+    {
+        /// <summary>
+        /// Load the media items pointed to by 'pathfilenames' into the 'Model'
+        /// </summary>
+        /// <param name="pathfilenames">a vector of strings; each string containing path/filename for an image file to be loaded</param>
+        /// <returns>the unique identifiers of the images that have been loaded</returns>
+        IList<String> load(IList<String> pathfilenames);
+
+        /// <summary>
+        /// Return a copy of the image specified by 'key', scaled according to the dimentsions of the visual container (ie frame) it will be viewed in.
+        /// </summary>
+        /// <param name="key">the unique identifier for the image to be returned</param>
+        /// <param name="frameWidth">the width (in pixels) of the 'frame' it is to occupy</param>
+        /// <param name="frameHeight">the height (in pixles) of the 'frame' it is to occupy</param>
+        /// <returns>the Image pointed identified by key</returns>
+        Image getImage(String key, int frameWidth, int frameHeight);
+    }
+}
---
+++ COMP3304Assessment/Model.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP3304Assessment
+{
+    class Model : IModel
+    {
+
+        public Model()
+        {
+
+        }
+
+        public IList<String> load(IList<String> pathfilenames)
+        {
+
+        }
+
+        public Image getImage(String key, int frameWidth, int frameHeight)
+        {
+
+        }
+
+    }
+}
