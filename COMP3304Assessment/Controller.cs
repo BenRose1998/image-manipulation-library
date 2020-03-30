f3fbd57cb0de873cf4dea876b2e19c09bf342e70
BenRose1998 <rosb1_17@uni.worc.ac.uk>
2020-03-30 14:16:25 +0100

    Removed redundant declaration as member variable in controller

--- COMP3304Assessment/Controller.cs
+++ COMP3304Assessment/Controller.cs
@@ -17,20 +17,20 @@ namespace COMP3304Assessment
     class Controller
     {
         // DECLARE an IModel interface for an instance of Model to be stored in, call it '_model':
-        private IModel _model;
+        //private IModel _model;
         // DECLARE an IFilePathAdder interface for an instance of FilePathHandler to be stored in, call it '_fileHandler':
-        private IFilePathAdder _fileHandler;
+        //private IFilePathAdder _fileHandler;
         // DECLARE an IImageDisplaySetter interface for an instance of ImageHandler to be stored in, call it '_imageHandler':
-        private IImageDisplaySetter _imageHandler;
+        //private IImageDisplaySetter _imageHandler;
 
         public Controller()
         {
-            // INSTANTIATE '_model', with a new instance of Model
-            _model = new Model();
-            // INSTANTIATE '_fileHandler', with a new instance of FilePathHandler, pass it a reference to '_model'
-            _fileHandler = new FilePathHandler(_model);
-            // INSTANTIATE '_imageHandler', with a new instance of ImageHandler, pass it a reference to '_fileHandler' & '_model'
-            _imageHandler = new ImageHandler(_fileHandler as IFilePathGetter, _model);
+            // DECLARE & INSTANTIATE '_model', with a new instance of Model
+            IModel _model = new Model();
+            // DECLARE & INSTANTIATE '_fileHandler', with a new instance of FilePathHandler, pass it a reference to '_model'
+            IFilePathAdder _fileHandler = new FilePathHandler(_model);
+            // DECLARE & INSTANTIATE '_imageHandler', with a new instance of ImageHandler, pass it a reference to '_fileHandler' & '_model'
+            IImageDisplaySetter _imageHandler = new ImageHandler(_fileHandler as IFilePathGetter, _model);
 
             /* 
              * Run the application and pass it a reference to a new ImageViewer form. 
